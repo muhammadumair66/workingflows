876d8b2a07e41db3d7d795870e64d0c1b9218ac9
Muhammad Umair <mohammad.umair66@gmail.com>
2015-04-19 10:48:42 +0500

    Concatinated javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
 var gulp= require('gulp'),
 gutil=require('gulp-util'),
-coffee=require('gulp-coffee');
+coffee=require('gulp-coffee'),
+concat=require('gulp-concat');
 var coffeesources=['components/coffee/tagline.coffee'];
+var jssources=['components/scripts/rclick.js',
+'components/scripts/pixgrid.js',
+'components/scripts/tagline.js',
+'components/scripts/template.js'
+
+
+];
 gulp.task('coffee',function(){
 gulp.src('coffeesources')
 .pipe(coffee({bare:true})
 	.on('error',gutil.log))
 	.pipe(gulp.dest('components/scripts'))
+});
+gulp.task('js',function(){
+	gulp.src(jssources)
+	.pipe(concat('script.js'))
+	.pipe(gulp.dest('builds/development/js'))
 });
